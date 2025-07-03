0d2690297ec9badfa171e12aca517db57afd8f18
Vitalii Lymar <32154871+Limpovit@users.noreply.github.com>
2025-07-03 14:38:05 +0300

    init

---
+++ bin/dart_collections_homework.dart
@@ -0,0 +1,5 @@
+import 'package:dart_collections_homework/dart_collections_homework.dart' as dart_collections_homework;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_collections_homework.calculate()}!');
+}
---
+++ test/dart_collections_homework_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_collections_homework/dart_collections_homework.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
